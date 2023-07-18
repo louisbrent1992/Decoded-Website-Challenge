228fa27945f8a79bec4671af314e6c8fd4c08488
unknown <louisbrent1992@gmail.com>
2023-07-17 17:00:27 -0700

    updated home video link

--- src/_Data.js
+++ src/_Data.js
@@ -1,78 +1,78 @@
 export const images = [
-  { id: 1, url: "", alt: "" },
-  { id: 2, url: "", alt: "" },
-  { id: 3, url: "", alt: "" },
-  { id: 4, url: "", alt: "" },
-  { id: 5, url: "", alt: "" },
-  { id: 6, url: "", alt: "" },
-  { id: 7, url: "", alt: "" },
-  { id: 8, url: "", alt: "" },
+	{ id: 1, url: "", alt: "" },
+	{ id: 2, url: "", alt: "" },
+	{ id: 3, url: "", alt: "" },
+	{ id: 4, url: "", alt: "" },
+	{ id: 5, url: "", alt: "" },
+	{ id: 6, url: "", alt: "" },
+	{ id: 7, url: "", alt: "" },
+	{ id: 8, url: "", alt: "" },
 ];
 export const articles = [
-  { id: 1, url: "", title: "NEW SITE, WHO DIS?", alt: "NEWS" },
+	{ id: 1, url: "", title: "NEW SITE, WHO DIS?", alt: "NEWS" },
 
-  {
-    id: 2,
-    url: "",
-    title:
-      "New work! Decoded has been hard at work making the work that works. See it now.",
-    alt: "WORK PROCESS",
-  },
-  {
-    id: 3,
-    url: "",
-    title:
-      "Fun fact: The Pink Shrimp is the official Decoded mascot. The more you know...",
-    alt: "FACTS",
-  },
-  {
-    id: 4,
-    url: "",
-    title:
-      "How long do you think people sit around reading chyrons? Expecting something really important to come from behind the wall over here, but then discovering that you waited forever for something you could probably read on the internet in a matter of seconds. But no! You wanted to wait? Worth it? Maybe. What you waited for? Blue whales consume 500,000 calories in one mouthful.",
-    alt: "FACTS",
-  },
+	{
+		id: 2,
+		url: "",
+		title:
+			"New work! Decoded has been hard at work making the work that works. See it now.",
+		alt: "WORK PROCESS",
+	},
+	{
+		id: 3,
+		url: "",
+		title:
+			"Fun fact: The Pink Shrimp is the official Decoded mascot. The more you know...",
+		alt: "FACTS",
+	},
+	{
+		id: 4,
+		url: "",
+		title:
+			"How long do you think people sit around reading chyrons? Expecting something really important to come from behind the wall over here, but then discovering that you waited forever for something you could probably read on the internet in a matter of seconds. But no! You wanted to wait? Worth it? Maybe. What you waited for? Blue whales consume 500,000 calories in one mouthful.",
+		alt: "FACTS",
+	},
 ];
 
 export const videos = [
-  {
-    id: 1,
-    url: "https://decodedadvertising.com/uploads/images/home/Decoded_AgencyReel_2560x1080_halftext_v4-v1_LO.mp4",
-    title: "introduction",
-  },
-  {
-    id: 2,
-    url: "",
-    title: "",
-  },
-  {
-    id: 3,
-    url: "",
-    title: "",
-  },
-  {
-    id: 4,
-    url: "",
-    title: "",
-  },
-  {
-    id: 5,
-    url: "",
-    title: "",
-  },
-  {
-    id: 6,
-    url: "",
-    title: "",
-  },
-  {
-    id: 7,
-    url: "",
-    title: "",
-  },
-  {
-    id: 8,
-    url: "",
-    title: "",
-  },
+	{
+		id: 1,
+		url: "https://servd-decoded-advertising.b-cdn.net/production/images/home/Decoded_AgencyReel_2560x1080_halftext_v4-v1_LO.mp4",
+		title: "introduction",
+	},
+	{
+		id: 2,
+		url: "",
+		title: "",
+	},
+	{
+		id: 3,
+		url: "",
+		title: "",
+	},
+	{
+		id: 4,
+		url: "",
+		title: "",
+	},
+	{
+		id: 5,
+		url: "",
+		title: "",
+	},
+	{
+		id: 6,
+		url: "",
+		title: "",
+	},
+	{
+		id: 7,
+		url: "",
+		title: "",
+	},
+	{
+		id: 8,
+		url: "",
+		title: "",
+	},
 ];
